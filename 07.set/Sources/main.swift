ff0079278a1eca3344c781405fe99b75dfdf0530
Rezaul Karim <rezaul@orangetoolz.com>
2025-06-25 16:37:47 +0600

    add

---
+++ 07.set/Sources/main.swift
@@ -0,0 +1,8 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+// create set
+let names: Set = ["Alice", "Bob", "Charlie", "rezaul", "rezaul"];
+
+print(names.contains("rezaul"))
+print(names)
